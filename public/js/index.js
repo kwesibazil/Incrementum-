f90c72cc4f6251db9c6e5727894e4522ccce5cf0
kwesi Bazil <kwesibazil@gmail.com>
2020-12-31 03:47:04 -0400

    Addded transition to vertical navbar

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,20 @@
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const today = new Date();
-const username = 'username';
+const toggleBtn = document.getElementById("toggler-icon-modal");
+const modal = document.getElementById("myModal");
+const modalDialog = document.getElementById("myModalDialog")
 
 
-//Ordinal Date (1st/2nd/3rd/..th)
-const getOrdinalDate = date => date+(31==date||21==date||1==date?"st":22==date||2==date?"nd":23==date||3==date?"rd":"th");
-
-//name of the month(january/february/march...)
-const getMonthName = (monthName, monthArr) => monthArr[monthName];
-
-document.getElementById('current-date').textContent = `Today, ${getOrdinalDate(today.getDate())} ${getMonthName(today.getMonth(), months)} ${today.getFullYear()} `;
-
-document.getElementById('current-username').innerHTML = `${username}`;
+toggleBtn.addEventListener('click', () => {
+  modal.classList.add("modal-show");
+  modalDialog.classList.add("modal-dialog-show");
+})
 
 
+modal.addEventListener('click', (e) => {
+  modalDialog.classList.remove("modal-dialog-show");
+  modal.classList.remove("modal-show");
+
+  // setTimeout(() => {
+  //   modal.classList.remove("modal-show");
+  // }, 200);
+ /**/
+});
